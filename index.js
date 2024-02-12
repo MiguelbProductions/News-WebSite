ab0761cc9b9892325ee7954547ae3a9ec6f78087
Miguel <miguellbproductions@gmail.com>
2024-02-12 03:48:30 +0000

    Static Part 1/2

--- index.js
+++ index.js
@@ -12,8 +12,18 @@ app.use(bodyParser.urlencoded({
 
 app.engine("html", require("ejs").renderFile)
 app.set("view engine", "html")
-app.use("public", express.static(path.join(__dirname, "public")))
+app.use("/public", express.static(path.join(__dirname, "public")))
 app.set("views", path.join(__dirname, "/views"))
+
+app.get("/", (req, res) => {
+
+    if (req.query.search != null) res.send(`Searching... (${req.query.search})`)
+    else res.render("Home.ejs", {})
+})
+
+app.get("/:slug", (req, res) => {
+    res.send(req.params.slug)
+})
 
 const PORT = 7001
 app.listen(PORT, () => {
